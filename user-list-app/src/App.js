20666c46192b8a0aebe09b9f71a763302fa3abfb
ONS BABA <nousababa72@gmail.com>
2024-10-29 19:44:38 +0100

    first commit

---
+++ user-list-app/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+import UserList from './UserList';
+import './App.css'; // You can style your App here
+
+const App = () => {
+  return (
+    <div className="App">
+      <UserList />
+    </div>
+  );
+};
+
+export default App;
